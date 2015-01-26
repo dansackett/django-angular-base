e324f0fa4631398a2dfc9226e5f3ad47b1284940
Dan Sackett <danesackett@gmail.com>
2015-01-25 22:40:48 -0500

    Add account API stuff for CRUD, login, and logout

--- src/apps/authentication/views.py
+++ src/apps/authentication/views.py
@@ -1,3 +1,88 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.contrib.auth import authenticate, login, logout
+
+from rest_framework import permissions, viewsets, status, views
+from rest_framework.response import Response
+
+from authentication.models import Account
+from authentication.permissions import IsAccountOwner
+from authentication.serializers import AccountSerializer
+
+class AccountViewSet(viewsets.ModelViewSet):
+    """
+    The main vieweset for viewing, editing, creating, and deleting account
+    objects.
+    """
+    lookup_field = 'username'
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
+    def get_permissions(self):
+        if self.request.method in permissions.SAFE_METHODS:
+            return (permissions.AllowAny(),)
+
+        if self.request.method == 'POST':
+            return (permissions.AllowAny(),)
+
+        return (permissions.IsAuthenticated(), IsAccountOwner(),)
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            account = Account.objects.create_user(**serializer.DATA)
+
+            account.set_password(request.DATA.get('password'))
+            account.save()
+            return Response(serializer.DATA, status=status.HTTP_201_CREATED)
+
+        context = {
+            'status': 'Bad request',
+            'message': 'Account could not be created with received data.'
+        }
+
+        return Response(context, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(views.APIView):
+    """
+    View for logging a user in.
+    """
+    def post(self, request, format=None):
+        data = json.loads(request.body)
+        email = data.get('email', None)
+        password = data.get('password', None)
+        account = authenticate(email=email, password=password)
+
+        if account is not None:
+            if account.is_active:
+                login(request, account)
+                serialized = AccountSerializer(account)
+
+                return Response(serialized.data)
+            else:
+                context = {
+                    'status': 'Unauthorized',
+                    'message': 'This account has been disabled.'
+                }
+
+                return Response(context, status=status.HTTP_401_UNAUTHORIZED)
+
+        context = {
+            'status': 'Unauthorized',
+            'message': 'Username/password combination invalid.'
+        }
+
+        return Response(context, status=status.HTTP_401_UNAUTHORIZED)
+
+
+class LogoutView(views.APIView):
+    """
+    View for logging a user out.
+    """
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request, format=None):
+        logout(request)
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
--- src/core/urls.py
+++ src/core/urls.py
@@ -1,8 +1,17 @@
 from django.conf.urls import patterns, include, url
 
+from rest_framework import routers
+
+from authentication.views import AccountViewSet, LoginView
+
+router = routers.SimpleRouter()
+router.register(r'accounts', AccountViewSet)
+
 urlpatterns = patterns('',
     # url(r'^$', 'src.core.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api/v1/', include(router.urls)),
+    url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
+    url(r'^api/v1/auth/logout/$', LogoutView.as_view(), name='logout'),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 )
